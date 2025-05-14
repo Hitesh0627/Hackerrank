4ee8eeae68c91fd5da123db6f3972716fc1e4590
Hitesh0627 <103327522+Hitesh0627@users.noreply.github.com>
2025-05-14 15:46:38 +0530

    [Hitesh] Add: Merge sorted lists

---
+++ MergeSortedLists.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class MergeSortedLists {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        LinkedList<Integer> list1 = new LinkedList<>();
+        for (int i = 0; i < n1; i++) {
+            list1.add(sc.nextInt());
+        }
+
+        int n2 = sc.nextInt();
+        LinkedList<Integer> list2 = new LinkedList<>();
+        for (int i = 0; i < n2; i++) {
+            list2.add(sc.nextInt());
+        }
+
+        LinkedList<Integer> merged = new LinkedList<>();
+        int i = 0, j = 0;
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i) <= list2.get(j)) {
+                merged.add(list1.get(i++));
+            }
+            else {
+                merged.add(list2.get(j++));
+            }
+        }
+
+        // Add remaining elements
+        while (i < list1.size()) {
+            merged.add(list1.get(i++));
+        }
+        while (j < list2.size()) {
+            merged.add(list2.get(j++));
+        }
+
+        // Output
+        System.out.println("Merged Sorted List:");
+        for (int num : merged) {
+            System.out.print(num + " ");
+        }
+
+    }
+}
